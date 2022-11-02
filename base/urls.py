89b8345e02e412701eebd7addb13a7e401d441ea
Jose Gomez <pipoe2h@gmail.com>
2022-11-02 22:06:19 +0000

    Update: missing libraries

--- base/urls.py
+++ base/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
