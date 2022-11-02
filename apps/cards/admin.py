bd0a23eec6ccf38eea7731ec9929bc190b076bad
Jose Gomez <pipoe2h@gmail.com>
2022-11-02 22:01:44 +0000

    Added: Admin panel view

--- apps/cards/admin.py
+++ apps/cards/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+
 from .models import Card
 
-# Register your models here.
 
-admin.site.register(Card)
+@admin.action(description='Mark selected cards as visible')
+def make_visible(modeladmin, request, queryset):
+    queryset.update(visible=True)
+
+
+@admin.action(description='Mark selected cards as invisible')
+def make_invisible(modeladmin, request, queryset):
+    queryset.update(visible=False)
+
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'visible')
+    actions = [make_visible, make_invisible]
+
+
+admin.site.register(Card, CardAdmin)
